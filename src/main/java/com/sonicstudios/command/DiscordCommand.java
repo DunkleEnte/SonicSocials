3ef2616be115870775469ca732b3acd5eae3fe56
DunkleEnte <ngkenrcx@gmail.com>
2026-03-11 23:18:46 +0100

    Refactor type check for CommandSender to use final

--- src/main/java/com/sonicstudios/command/DiscordCommand.java
+++ src/main/java/com/sonicstudios/command/DiscordCommand.java
@@ -18,7 +18,7 @@ public class DiscordCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        if(!(sender instanceof Player player)) return false;
+        if(!(sender instanceof final Player player)) return false;
 
         if(MessageUtil.getChat()) {
             player.sendMessage(MessageUtil.getPrefix() + MessageUtil.get("sonicsocials.discord-link"));
